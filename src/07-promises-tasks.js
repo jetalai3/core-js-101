c6cb903034c158596072d497a2a0c3936de555b9
Viktar Svintsitski <jetalai3@gmail.com>
2019-12-01 23:40:46 +0300

    feat: complete some more tests

--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -28,8 +28,15 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return Promise.resolve(isPositiveAnswer).then((value) => {
+    if (value === true) {
+      return 'Hooray!!! She said "Yes"!';
+    } if (value === false) {
+      return 'Oh no, she said "No".';
+    }
+    return Promise.reject(new Error('Wrong parameter is passed! Ask her again.'));
+  });
 }
 
 
@@ -48,8 +55,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
