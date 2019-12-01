0c324db2e1389cc0425e1d103662ed5988e8d041
Viktar Svintsitski <jetalai3@gmail.com>
2019-12-01 21:33:26 +0300

    feat: fix warnings

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-names */
 /* ************************************************************************************************
  *                                                                                                *
  * Plese read the following tutorial before implementing tasks:                                   *
--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-names */
 /* eslint-disable no-param-reassign */
 /* eslint-disable consistent-return */
 /* eslint-disable prefer-rest-params */
